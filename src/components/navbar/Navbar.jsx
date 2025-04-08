2f5d16dd86cd386ef663d901ff84724eacda2387
Nilesh Mishra <thenileshmishra@gmail.com>
2025-04-08 16:06:41 +0530

    drop down issue fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Select } from 'antd';
 import './Navbar.css';
 
 function Navbar({ selectedSheet, onSheetChange, isDarkMode, toggleDarkMode }) {
   const { Option } = Select;
+  const location = useLocation();
+  
+  // Check if we're on the questions page
+  const isQuestionsPage = location.pathname.includes('/questions/');
 
   return (
     <nav className="navbar">
@@ -14,18 +18,20 @@ function Navbar({ selectedSheet, onSheetChange, isDarkMode, toggleDarkMode }) {
         </Link>
         
         <div className="navbar__right">
-          <div className="navbar__sheet-selector">
-            <Select
-              value={selectedSheet}
-              onChange={onSheetChange}
-              className="navbar__select"
-              dropdownClassName="navbar__select-dropdown"
-            >
-              <Option value="sheet3">Love Babar</Option>
-              <Option value="sheet1">Arsh</Option>
-              <Option value="sheet2">Fraz</Option>
-            </Select>
-          </div>
+          {!isQuestionsPage && (
+            <div className="navbar__sheet-selector">
+              <Select
+                value={selectedSheet}
+                onChange={onSheetChange}
+                className="navbar__select"
+                dropdownClassName="navbar__select-dropdown"
+              >
+                <Option value="sheet3">Love Babar</Option>
+                <Option value="sheet1">Arsh</Option>
+                <Option value="sheet2">Fraz</Option>
+              </Select>
+            </div>
+          )}
           
           <button 
             className="navbar__darkmode" 
